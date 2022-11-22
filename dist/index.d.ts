f1ae8f5784c7f56617baa3c10a0ca32ecc156111
Jonathan Ehman <jonathan.ehman@mdaemon.com>
2022-11-22 08:00:42 -0600

    One more update to TS declarations

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,20 @@
-declare class Emitter {}
-export default Emitter;
+import { off } from "process";
+
+export = Emitter;
+
+declare class Emitter {
+  register(name: string, namespace: string, func: ((data: any) => any)): void;
+  on(name: string, namespace: string, func: ((data: any) => any)): void;
+  subscribe(name: string, namespace: string, func: ((data: any) => any)): void;
+  once(name: string, func: ((data: any) => any)): void;
+  onMany(namespace: string, obj: any): void;
+  unregister(name: string, namespace: string): void;
+  off(name: string, namespace: string): void;
+  unsubscribe(name: string, namespace: string): void;
+  offAll(namespace: string): void;
+  trigger(name: string, data?: any): void;
+  emit(name: string, data?: any): void;
+  publish(name: string, data?: any): void;
+  propagate(data: any, name: string): void;
+  isRegistered(name: string, namespace: string): boolean;
+}
