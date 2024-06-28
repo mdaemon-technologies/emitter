e4e0daf46c8beeff6815b2b75f3a7f1389f53e65
MDaemon Technologies, Ltd. <jonathan.ehman@mdaemon.com>
2024-06-28 08:22:58 -0500

    Updated TypeScript declaration
    The TypeScript declaration was not consistent with the JavaScript behavior. Made appropriate corrections to the register/unregister methods and alias.

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,14 +1,14 @@
 export = Emitter;
 
 declare class Emitter {
-  register(name: string, namespace: string, func: ((data: any) => any)): void;
-  on(name: string, namespace: string, func: ((data: any) => any)): void;
-  subscribe(name: string, namespace: string, func: ((data: any) => any)): void;
+  register(name: string, namespace: string | ((data: any) => any), func?: ((data: any) => any) = undefined): void;
+  on(name: string, namespace: string | ((data: any) => any), func?: ((data: any) => any) = undefined): void;
+  subscribe(name: string, namespace: string | ((data: any) => any), func?: ((data: any) => any) = undefined): void;
   once(name: string, func: ((data: any) => any)): void;
   onMany(namespace: string, obj: any): void;
-  unregister(name: string, namespace: string): void;
-  off(name: string, namespace: string): void;
-  unsubscribe(name: string, namespace: string): void;
+  unregister(name: string, namespace?: string = "all"): void;
+  off(name: string, namespace?: string = "all"): void;
+  unsubscribe(name: string, namespace?: string = "all"): void;
   offAll(namespace: string): void;
   trigger(name: string, data?: any): void;
   emit(name: string, data?: any): void;
