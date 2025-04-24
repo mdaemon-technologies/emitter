9bb21bdee42fcd69ec4513c03359eaad173f8233
MDaemon Technologies, Ltd. <jonathan.ehman@mdaemon.com>
2025-04-24 10:22:01 -0500

    Updated packages
    installed the latest packages

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import typescript from "@rollup/plugin-typescript";
 import commonjs from '@rollup/plugin-commonjs';
 import terser from '@rollup/plugin-terser';
-import pkg from './package.json';
+import pkg from './package.json' with { type: "json" };
 
 export default [
   {
